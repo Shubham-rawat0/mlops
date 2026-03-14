6633cb2c4bdd6163a27fafcae1086090eb37a181
Shubham-rawat0 <rawat07shubham@gamil.com>
2026-03-14 08:35:03 +0530

    data transformation

--- app.py
+++ app.py
@@ -3,14 +3,20 @@ sys.path.insert(0, '.')
 
 from src.mlops.logger import logging
 from src.mlops.exception import CustomException
+
 from src.mlops.components.data_ingestion import DataIngestion
+from src.mlops.components.data_transformation import DataTransformationConfig
+from src.mlops.components.data_transformation import DataTransformation
 
 if __name__ == "__main__":
     logging.info("The app has started")
 
     try:
         data_ingestion=DataIngestion()
-        data_ingestion.initiate_data_ingestion()
+        train_data_path,test_data_path=data_ingestion.initiate_data_ingestion()
+
+        data_transformation=DataTransformation()
+        data_transformation.initiate_data_transformation(train_data_path,test_data_path)
 
     except Exception as e:
         raise CustomException(e,sys) 
--- src/mlops/utils.py
+++ src/mlops/utils.py
@@ -3,6 +3,7 @@ import sys
 from .exception import CustomException
 from .logger import logging
 import pandas as pd
+import pickle
 
 def read_csv_data():
     file_path = os.path.join("notebook", "data", "raw.csv")
@@ -15,3 +16,15 @@ def read_csv_data():
 
     except Exception as e:
         raise CustomException(e, sys)
+    
+def save_objects(file_path , obj):
+    try:
+        dir_path=os.path.dirname(file_path)
+
+        os.makedirs(dir_path,exist_ok=True)
+
+        with open(file_path,"wb") as file_obj:
+            pickle.dump(obj,file_obj)
+
+    except Exception as e:
+        raise CustomException(e,sys)
